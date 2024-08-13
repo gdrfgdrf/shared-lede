aaeedcad47ff2a591d95d1cb8458ce0283e6cec6
gdrfgdrf <65430754+gdrfgdrf@users.noreply.github.com>
2024-08-14 05:01:06 +0800

    Update 1.modify.sh

--- preset-lede/1.modify.sh
+++ preset-lede/1.modify.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # modify login IP
-#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # modify host name
-#sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/Phicomm-Router/g' package/base-files/files/bin/config_generate
 
 # modify device name show
-#sed -i 's/Xiaomi Mi Router 4A Gigabit Edition/Xiaomi 4A Gigabit/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts
+sed -i 's/Xiaomi Mi Router 4A Gigabit Edition/Xiaomi 4A Gigabit/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts
 #sed -i 's/Xiaomi Mi Router 3G v2/Xiaomi 3G v2/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g-v2.dts
 #sed -i 's/Xiaomi Redmi Router AC2100/Redmi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_redmi-router-ac2100.dts
 #sed -i 's/Xiaomi Mi Router AC2100/Xiaomi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-ac2100.dts
