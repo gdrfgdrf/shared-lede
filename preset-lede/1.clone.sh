61a4f1903e4ea9cd9adf9def86800afe86b55c30
gdrfgdrf <65430754+gdrfgdrf@users.noreply.github.com>
2024-08-14 17:36:37 +0800

    Update 1.clone.sh

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -12,7 +12,6 @@ cat /dev/null > feeds.conf.default
 
 echo "src-git packages https://github.com/openwrt/packages.git^openwrt-23.05" >> feeds.conf.default
 echo "src-git packages_lienol https://github.com/Lienol/openwrt-packages.git^23.05" >> feeds.conf.default
-echo "src-git node https://github.com/nxhack/openwrt-node-packages.git^openwrt-23.05" >> feeds.conf.default
 echo "src-git luci https://github.com/Lienol/openwrt-luci^23.05" >> feeds.conf.default
 echo "src-git luci_lede https://github.com/coolsnowwolf/luci.git;openwrt-23.05" >> feeds.conf.default
 echo "src-git-full routing https://git.openwrt.org/feed/routing.git^openwrt-23.05" >> feeds.conf.default
